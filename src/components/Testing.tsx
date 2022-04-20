50380e69897ba8dd0b897d18b6a44f4f44797aae
OleksandrDanylchenko <olexander.danilchenko@gmail.com>
2022-04-20 20:21:52 +0300

    feat: add `ThemeProvider`

--- src/components/Testing.tsx
+++
@@ -1,5 +0,0 @@
-const Testing = () => {
-  return <h1>Hero!</h1>;
-};
-
-export default Testing;
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,25 @@
-import './index.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from './App';
+import { CssBaseline, StyledEngineProvider, ThemeProvider } from '@mui/material';
+
+import theme from '@styles/theme';
 
 const container = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <CssBaseline enableColorScheme />
+        <h1>Hello there! There is a roboto</h1>
+      </ThemeProvider>
+    </StyledEngineProvider>
   </React.StrictMode>,
 );
