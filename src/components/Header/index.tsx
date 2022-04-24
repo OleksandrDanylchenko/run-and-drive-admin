cae15c201e6851926025fd51a54769114aa2d766
OleksandrDanylchenko <olexander.danilchenko@gmail.com>
2022-04-24 20:05:39 +0300

    feat: add `Account settings` button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,30 @@
 import { FC } from 'react';
 
+import Logout from '@mui/icons-material/Logout';
 import MapIcon from '@mui/icons-material/Map';
+import Settings from '@mui/icons-material/Settings';
 import AppBar from '@mui/material/AppBar';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Divider from '@mui/material/Divider';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 import Toolbar from '@mui/material/Toolbar';
+import Tooltip from '@mui/material/Tooltip';
 import Typography from '@mui/material/Typography';
+import { useEventElement } from 'run-and-drive-lib/hooks';
 
-import { HeaderTitle, HeaderToolbar } from './styles';
+import { AccountAvatar, AccountButton, HeaderTitle, HeaderToolbar } from './styles';
 
 const Header: FC = () => {
+  const { element, handleClick, handleClose } = useEventElement();
+  const menuOpen = Boolean(element);
+
+  const getAvatar = (type: 'header' | 'list') => (
+    <Avatar src="/test-image" alt="Oleksandr" css={AccountAvatar} className={type} />
+  );
+
   return (
     <AppBar>
       <Toolbar css={HeaderToolbar}>
@@ -15,7 +32,43 @@ const Header: FC = () => {
           <MapIcon />
           Run & Drive
         </Typography>
+        <Tooltip title="Account settings">
+          <Button
+            aria-controls={menuOpen ? 'account-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={menuOpen ? 'true' : undefined}
+            onClick={handleClick}
+            css={AccountButton}
+          >
+            Oleksandr D.
+            {getAvatar('header')}
+          </Button>
+        </Tooltip>
       </Toolbar>
+      <Menu
+        anchorEl={element}
+        id="account-menu"
+        open={menuOpen}
+        onClose={handleClose}
+        onClick={handleClose}
+      >
+        <MenuItem>
+          <ListItemIcon>{getAvatar('list')}</ListItemIcon> My Profile
+        </MenuItem>
+        <Divider />
+        <MenuItem>
+          <ListItemIcon>
+            <Settings fontSize="small" />
+          </ListItemIcon>
+          Settings
+        </MenuItem>
+        <MenuItem>
+          <ListItemIcon>
+            <Logout fontSize="small" />
+          </ListItemIcon>
+          Logout
+        </MenuItem>
+      </Menu>
     </AppBar>
   );
 };
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,8 +1,10 @@
 import { css } from '@emotion/react';
+import { Theme } from '@mui/material';
 import { flexbox, pxToRem } from 'run-and-drive-lib/styles';
 
 export const HeaderToolbar = css`
-  gap: ${pxToRem('10px')};
+  justify-content: space-between;
+  gap: 10px;
 `;
 
 export const HeaderTitle = css`
@@ -11,8 +13,24 @@ export const HeaderTitle = css`
   font-weight: 500;
 `;
 
-export const HeaderButton = css`
-  font-size: 1.2em;
+export const AccountButton = css`
+  text-transform: inherit;
   color: inherit;
-  text-transform: none;
+  gap: 10px;
+`;
+
+export const AccountAvatar = (theme: Theme) => css`
+  --size: 0;
+
+  width: var(--size);
+  height: var(--size);
+  background-color: ${theme.palette.info.dark};
+
+  &.header {
+    --size: 32px;
+  }
+
+  &.list {
+    --size: 25px;
+  }
 `;
