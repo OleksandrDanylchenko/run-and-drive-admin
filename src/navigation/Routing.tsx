9aa05651de1ddd80168abf12d31ffd7df98f44da
OleksandrDanylchenko <olexander.danilchenko@gmail.com>
2022-05-13 19:55:19 +0300

    feat: add usage of protected route

--- src/navigation/Routing.tsx
+++ src/navigation/Routing.tsx
@@ -1,15 +1,23 @@
 import React, { FC } from 'react';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
+import { ProtectedRoute } from 'run-and-drive-lib/router';
+
 import Dashboard from '@pages/Dashboard';
 import Home from '@pages/Home';
 
 const Routing: FC = () => (
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Home />}>
-        <Route index element={<Dashboard />} />
+      <Route element={<ProtectedRoute isAllowed={true} redirectPath="/" />}>
+        <Route path="/login" element={<h2>Hello, login screen!</h2>} />
       </Route>
+      <Route element={<ProtectedRoute isAllowed={false} redirectPath="/login" />}>
+        <Route path="/" element={<Home />}>
+          <Route index element={<Dashboard />} />
+        </Route>
+      </Route>
+      <Route element={<Navigate to="/" />} />
       <Route path="/*" element={<Navigate to="/" />} />
     </Routes>
   </BrowserRouter>
